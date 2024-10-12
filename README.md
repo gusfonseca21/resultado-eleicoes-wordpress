cb707d9e060909e915d20e132dc222b12354f11c
Gustavo <gusfonseca.dev@gmail.com>
2024-10-11 23:52:23 -0300

    Primeiro Commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# Adicionando projeto Vanilla JS no WordPress
+
+#vanillajs #wordpress
+
+**IMPORTANTE: NÃO SE ESQUEÇA DE FAZER O BACKUP DO SITE ANTES DE APLICAR ESSAS MUDANÇAS NO AMBIENTE DE PRODUÇÃO**
+
+Em um arquivo HTML podemos adicionar código JavaScript e CSS, porém, ao adicionar um código HTML com script JS pelo Elementor, por exemplo, o WordPress bloqueia a execução do código JS por motivos de segurança. Para fazer a adição de código JS customizado em uma página no WordPress precisamos seguir alguns passos.
+
+### Passo 1: criar um tema filho (_child theme_)
+
+Os temas (no menu de **Aparência**) são importantes pois será neles que poderemos adicionar e gerenciar código JavaScript customizado. É importante criar um tema filho pois ele herdará o tema definido pelo pai e quando o tema principal for atualizado, a nossa modificação de importação do código JS não será sobrescrito.
+
+TERMINAR ESSA PARTE DA DOCUMENTAÇÃO QUANDO CRIAR UM TEMA FILHO
+
+https://wordpress.com/support/themes/child-themes/
+
+### Passo 2: adicionar o código JavaScript dentro do diretório
+
+Agora precisamos que o código JS esteja disponível dentro do nosso ambiente WordPress. O acesso ao gerenciamento de pastas e arquivos do WordPress pode variar e pode ser necessário contatar o gerenciador do WordPress para realizar esse passo.
+
+No caminho `/public_html/wp-content/themes/NOME_DO_TEMA_FILHO/assets/js/` adicione os arquivos que serão utilizados pelo projeto.
+
+### Passo 3: referencie o código JS em _functions.php_
+
+Para que seja possível utilizar o código JS, o WordPress precisa que ele seja referenciado no arquivo _functions.php_.
+
+**Faça um backup do arquivo _functions.php_ para evitar problemas**
+
+No menu de **Aparência**, selecione o submenu **Editor de arquivos de tema**. Em seguida selecione o tema filho para editar. Você verá as pastas e arquivos que esse tema tem disponível. Note que é possível editar os arquivos de código nessa parte, se eles já estiverem sido adicionados no servidor. Selecione o arquivos _functions.php_ para editar.
+
+Dentro de _functions.php_ vá ao final do arquivo e adicione o seguinte código:
+
+```php
+
+function NOME_FUNCAO() {
+	if(TAG_CONDICIONAL(ID_PAGINA)) {
+		wp_enqueue_script('DESCRIÇÃO_CÓDIGO', get_template_directory_uri() . '/assets/js/NOME_ARQUIVO.js', array(), "CACHE_BUSTING", false);
+	}
+}
+
+add_action('wp_enqueue_scripts', 'NOME_FUNCAO');
+```
+
+- **_NOME_FUNCAO_**: Defina o nome da função em
+
+- **_TAG_CONDICIONAL_**: Escolha a tag condicional que melhor condiz com o problema que deseja resolver. Essa tag pode servir para escolher uma página específica aonde será importado o código JS, para definir que ele será importado na página principal, entre outros. Para ver quais tags condicionais estão disponíveis, veja: https://developer.wordpress.org/themes/basics/conditional-tags/
+
+- **_ID_PAGINA_**: _TAG_CONDICIONAL_ é um método, que pode aceitar um argumento. Se, por exemplo, selecionarmos a tag condicional **is_page()**, significa que queremos que o código JS seja importado em uma página específica. Para isso passamos como argumento o ID da página. Para saber qual o ID da página, vá no menu principal do WordPress, selecione **Páginas** e busque a a página que deseja. Ao clicar nela, você verá na URL do navegador o endereço da página com a seguinte query string `?post=ID`. Id será passado como argumento em is_page().
+
+- **_DESCRIÇÃO_CÓDIGO_**: Faça uma descrição da funcionalidade do código.
+
+- **_NOME_ARQUIVO_**: Nessa parte você verá `get_template_directory_uri() . '/assets/js/NOME_ARQUIVO.js'` . Esta parte serve para adicionar o caminho do arquivo a ser importado dentro do servidor. No caso específico utilizamos `get_template_directory_uri() .` para concatenar as strings do caminho do código.
+
+- **_CACHE_BUSTING_**: Cache Butsting é uma estratégia para garantir que os usuários do site sempre tenham a versão mais atual dos arquivos, evitando que a versão do arquivo seja puxado pelo cache, em vez da versão mais atual. Para mais informações sobre essa estratégia, veja: https://www.keycdn.com/support/what-is-cache-busting. É importante saber que, apesar do artigo não recomendar o método de query string para realizar o cache busting, essa é a estratégia utilizada pelo WordPress. **TODA A VEZ QUE REALIZAR ALGUMA ALTERAÇÃO NO CÓDIGO JAVASCRIPT, É NECESSÁRIO GARANTIR QUE O CACHE BUSTING OCORRA, MODIFICANDO A STRING**.
+
+Para mais informações sobre o método **_wp_enqueue_script_**, veja: https://developer.wordpress.org/reference/functions/wp_enqueue_script/.
+
+### Passo 4: verifique se o código JS está corretamente importado
+
+Acesse a sua nova página criada e acesse o código fonte da página. Procure por alguma tag como `<script type="text/javascript" src="LINK_CODIGO_JS"></script>` . Clique no link do código e verifique se o código JS está correto.
+
+### Passo 4: adicione o código HTML à sua página
+
+Na nova página criada, usando o Elementor ou outra ferramenta, adicione a base do seu código HTML. É importante notar que na pré-visualização da página no Elementor, o código JS não é executado.
+
+Dentro do HTML coloque os scripts de importação de bibliotecas de JavaScript e CSS. É importante adicionar o CSS dentro do HTML para ser possível visualizar as alterações feitas no CSS em tempo real durante a edição do HTML pelo Elementor.
+
+### Passo 5: salve as alterações e teste a página
---
+++ resultado-eleicao.js
@@ -0,0 +1,281 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // --------->>>>>>> SELECIONDO OPÃ‡Ã•ES <<<<<------------
+  const painelEl = document.querySelector(".painel");
+  const seletorCandidaturaEl = document.querySelector(".seletor-candidatura");
+  const seletorEstadoEl = document.querySelector(".seletor-estado");
+  const seletorMunicipioEl = document.querySelector(".seletor-municipio");
+  const botaoCarregarEl = document.querySelector(".carregar");
+  const tituloColVice = document.querySelector(".vice-linha");
+  const tabelaEl = document.querySelector(".tabela");
+
+  let candidaturaSelecionada = "";
+  let estadoSelecionado = "";
+  let municipioSelecionado = "";
+
+  seletorCandidaturaEl.addEventListener("change", (value) => {
+    const valor = value.target.value;
+    if (!valor) {
+      seletorEstadoEl.disabled = true;
+      seletorMunicipioEl.disabled = true;
+      botaoCarregarEl.disabled = true;
+      limparOpcoes(seletorMunicipioEl);
+      candidaturaSelecionada = "";
+      seletorEstadoEl.value = "";
+    } else {
+      seletorEstadoEl.disabled = false;
+      candidaturaSelecionada = valor;
+    }
+  });
+
+  seletorEstadoEl.addEventListener("change", (value) => {
+    limparOpcoes(seletorMunicipioEl);
+    const valor = value.target.value;
+    if (!valor) {
+      seletorMunicipioEl.disabled = true;
+      botaoCarregarEl.disabled = true;
+      estadoSelecionado = "";
+    } else {
+      seletorMunicipioEl.disabled = false;
+      estadoSelecionado = valor;
+      retornaMunicipios(valor);
+    }
+  });
+
+  seletorMunicipioEl.addEventListener("change", (value) => {
+    const valor = value.target.value;
+    if (!valor) {
+      botaoCarregarEl.disabled = true;
+      municipioSelecionado = "";
+    } else {
+      botaoCarregarEl.disabled = false;
+      municipioSelecionado = valor;
+    }
+  });
+
+  function retornaMunicipios(siglaEstadoSelecionado) {
+    // Importando array com nome dos municÃ­pios e seu cÃ³digo do TSE
+    // eslint-disable-next-line no-undef
+    axios
+      .get(
+        "https://homolog.congressoemfoco.com.br/wp-content/themes/cef/assets/json/municipios_brasileiros_tse.json"
+      )
+      .then((resposta) => {
+        const arrayOpcoesMunicipios = resposta.data
+          .map((municipio) => {
+            if (municipio.uf.toLowerCase() === siglaEstadoSelecionado) {
+              const opcaoMunicipioEl = document.createElement("option");
+              let codigo_municipio = municipio.codigo_tse.toString();
+              if (codigo_municipio.length < 5) {
+                codigo_municipio = codigo_municipio.padStart(5, "0");
+              }
+              opcaoMunicipioEl.value = codigo_municipio;
+              opcaoMunicipioEl.textContent = municipio.nome_municipio;
+              return opcaoMunicipioEl;
+            }
+          })
+          .filter((opcaoMunicipioEl) => opcaoMunicipioEl);
+        arrayOpcoesMunicipios.forEach((opcao) => {
+          seletorMunicipioEl.appendChild(opcao);
+        });
+      });
+  }
+
+  function limparOpcoes(elementoASerLimpo) {
+    const opcoes = elementoASerLimpo.options;
+    // Remove todas as opÃ§Ãµes menos a primeira, que Ã© a padrÃ£o
+    for (let i = opcoes.length - 1; i > 0; i--) {
+      elementoASerLimpo.remove(i);
+    }
+  }
+
+  // ------->>>>> CARREGANDO E MOSTRANDO DADOS NA TABELA <<<<<------
+  const ENDPOINT_BASE =
+    "https://resultados.tse.jus.br/oficial/ele2024/619/dados/{ESTADO}/{ESTADO_COD_MU}-{TIPO_CANDIDATURA}-e000619-u.json";
+
+  botaoCarregarEl.addEventListener("click", async () => {
+    try {
+      // Limpa tabela para mostrar os novos dados selecionados
+      limparTabela();
+
+      mostrarCarregando();
+
+      const endpointFinal = ENDPOINT_BASE.replace("{ESTADO}", estadoSelecionado)
+        .replace("{ESTADO_COD_MU}", estadoSelecionado + municipioSelecionado)
+        .replace("{TIPO_CANDIDATURA}", candidaturaSelecionada);
+
+      // c0011 = prefeito; c0013 = vereador
+      if (candidaturaSelecionada === "c0011") {
+        tituloColVice.style.display = "table-cell";
+      } else {
+        tituloColVice.style.display = "none";
+      }
+
+      // eslint-disable-next-line no-undef
+      const resultado = await axios.get(endpointFinal);
+
+      const dados = resultado.data;
+
+      // AgremiaÃ§Ãµes sÃ£o as junÃ§Ãµes de vÃ¡rios partidos. ContÃ©m um array com os partidos que fazem parte da agremiaÃ§Ã£o, que contÃ©m um array com os candidados do partido
+      const agremiacoes = dados?.carg[0]?.agr;
+
+      for (const agr of agremiacoes) {
+        agr.par.forEach((partido) => {
+          const sigla_partido = partido.sg;
+          partido.cand.forEach((candidato) => {
+            const nome_candidato = formatarNome(candidato.nmu);
+            const eleito = candidato.e === "s";
+
+            const linha = [
+              nome_candidato,
+              sigla_partido,
+              candidato.dvt,
+              candidato.st,
+              candidato.vap,
+              candidato.pvap + "%",
+            ];
+
+            // Se for prefeito, adicionar nome do vice
+            if (candidaturaSelecionada === "c0011") {
+              const nome_vice = formatarNome(candidato.vs[0].nmu);
+              linha.push(nome_vice);
+            }
+
+            const linhaTabelaEl = document.createElement("tr");
+
+            const dadosLinhaEl = linha.map((dadoColuna, index) => {
+              const colunaEl = document.createElement("td");
+              if (index === 0 || index === 1) {
+                colunaEl.className = "nome-partido";
+              }
+              if (index === 2) {
+                colunaEl.className = "validade";
+              }
+              if (index === 3 && eleito) {
+                colunaEl.className = "eleito";
+              }
+              if (index === 4 || index === 5) {
+                colunaEl.className = "numero-percentual";
+              }
+              if (index === 6) {
+                colunaEl.className = "vice";
+              }
+              colunaEl.textContent = dadoColuna;
+              return colunaEl;
+            });
+
+            dadosLinhaEl.forEach((linhaEl) => {
+              linhaTabelaEl.appendChild(linhaEl);
+            });
+
+            tabelaEl.appendChild(linhaTabelaEl);
+          });
+        });
+      }
+      const linhasOrdenadas = ordernarTabela(tabelaEl.children);
+
+      limparTabela();
+
+      linhasOrdenadas.forEach((linha) => {
+        tabelaEl.appendChild(linha);
+      });
+
+      removerCarregando();
+
+      console.log("EstÃ¡ chegando aqui?");
+      tabelaEl.style.display = "table";
+    } catch (error) {
+      removerCarregando();
+      console.error("Erro ao fazer requisiÃ§Ã£o dos dados: ", error);
+      mostrarErro();
+    }
+  });
+
+  function formatarNome(nome) {
+    return nome
+      .replaceAll(",", "")
+      .replace(";", "")
+      .replace("&#186", "")
+      .replaceAll("&#09", "");
+  }
+
+  function limparTabela() {
+    tabelaEl.style.display = "none";
+
+    // Lembrando que .forEach() nÃ£o funciona com coleÃ§Ãµes de elementos HTML
+    const linhasEl = tabelaEl.children;
+    for (let i = linhasEl.length - 1; i > 0; i--) {
+      linhasEl[i].remove();
+    }
+
+    const erroElemento = document.querySelector(".erro");
+    if (erroElemento) {
+      erroElemento.remove();
+    }
+  }
+
+  function ordernarTabela(elementosLinhas) {
+    const linhasArray = Array.from(elementosLinhas);
+
+    // Remove o cabeÃ§alho da tabela
+    linhasArray.shift();
+
+    linhasArray.sort((a, b) => {
+      const eleitoA = a.children[3].innerText.includes("Eleito") ? 1 : 0;
+      const eleitoB = b.children[3].innerText.includes("Eleito") ? 1 : 0;
+
+      if (eleitoA === eleitoB) {
+        const totalVotosA = Number(a.children[4].innerText);
+        const totalVotosB = Number(b.children[4].innerText);
+
+        return totalVotosB - totalVotosA;
+      }
+
+      return eleitoB - eleitoA;
+    });
+
+    console.log("linhasArray", linhasArray);
+
+    // Formata os valores de votos com toLocaleString para exibiÃ§Ã£o
+    linhasArray.forEach((linha) => {
+      const votosCell = linha.children[4];
+      const votosNumericos = Number(votosCell.innerText.replace(/\./g, "")); // Remove pontos de milhares
+      votosCell.textContent = votosNumericos.toLocaleString("pt-BR"); // Formata para o padrÃ£o brasileiro
+    });
+
+    return linhasArray;
+  }
+
+  function mostrarCarregando() {
+    const carregandoElemento = document.querySelector(".carregando");
+
+    if (carregandoElemento) {
+      carregandoElemento.remove();
+    }
+
+    const elementoDiv = document.createElement("div");
+    elementoDiv.className = "carregando";
+    elementoDiv.innerText = "Carregando...";
+    painelEl.appendChild(elementoDiv);
+  }
+
+  function removerCarregando() {
+    const carregandoElemento = document.querySelector(".carregando");
+
+    if (carregandoElemento) {
+      carregandoElemento.remove();
+    }
+  }
+
+  function mostrarErro() {
+    const erroElemento = document.querySelector(".erro");
+
+    if (erroElemento) {
+      erroElemento.remove();
+    }
+    const elementoDiv = document.createElement("div");
+    elementoDiv.className = "erro";
+    elementoDiv.innerText =
+      "Erro ao retornar dados da votaÃ§Ã£o. Por favor tente novamente mais tarde.";
+    painelEl.appendChild(elementoDiv);
+  }
+});
